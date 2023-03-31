cff7e9d70ece43427bea0412476154ea6a365a80
Ishini Hathurusinghe <ishinihathurusinghe@gmail.com>
2023-03-31 20:16:56 +0530

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,38 @@
+void main() {
+  String day = "Sunday";
+
+  switch(day){
+
+    case "Monday":
+    print("No clz");
+    break;
+
+    case "Tuesday":
+    print("No clz");
+    break;
+
+    case "Wedanesday":
+    print("No clz");
+    break;
+
+    case "Thurday":
+    print("No clz");
+    break;
+
+    case "Friday":
+    print("No clz");
+    break;
+
+    case "Saturday":
+    print("No clz");
+    break;
+
+    default:
+    print("Today you have flutter clz");
+
+  }
+  
+  }
+
+
+
